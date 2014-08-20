de639cc1b2f3faba1ab7474e6532b0fb7cb6165d
Mark Simpson <verdammelt@gmail.com>
2014-08-20 16:24:28 -0400

    Fixing project description string.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ibm-80 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A sorting machine like the IBM-80 series."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
