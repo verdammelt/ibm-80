750caf31c9b91861f504009a0fff48b25b8d9488
Mark Simpson <verdammelt@gmail.com>
2014-08-20 14:51:23 -0400

    Using CLJX.

--- project.clj
+++ project.clj
@@ -3,4 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[com.keminglabs/cljx "0.4.0"]]
+
+  :source-paths ["target/src/clj"]
+  :test-paths ["target/test/clj"]
+
+  :hooks [cljx.hooks]
+
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/src/clj"
+                   :rules :clj}
+                  {:source-paths ["test/cljx"]
+                   :output-path "target/test/clj"
+                   :rules :clj}]})
+
