266f94d90b526f17e2647345f67fdd4bf35699e7
Mark Simpson <verdammelt@gmail.com>
2014-08-20 15:30:12 -0400

    Adding cljsbuild.

--- project.clj
+++ project.clj
@@ -3,18 +3,43 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :plugins [[com.keminglabs/cljx "0.4.0"]]
 
   :source-paths ["target/src/clj"]
   :test-paths ["target/test/clj"]
 
-  :hooks [cljx.hooks]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2311"]]
+  :plugins [[com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
+            [lein-cljsbuild "1.0.3"]
+            [com.cemerick/clojurescript.test "0.3.1"]]
 
-  :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "target/src/clj"
-                   :rules :clj}
-                  {:source-paths ["test/cljx"]
-                   :output-path "target/test/clj"
-                   :rules :clj}]})
+  :hooks [cljx.hooks leiningen.cljsbuild]
 
+  :cljx 
+  {:builds [{:source-paths ["src/cljx"]
+             :output-path "target/src/clj"
+             :rules :clj}
+            {:source-paths ["src/cljx"]
+             :output-path "target/src/cljs"
+             :rules :cljs}
+            {:source-paths ["test/cljx"]
+             :output-path "target/test/clj"
+             :rules :clj}
+            {:source-paths ["test/cljx"]
+             :output-path "target/test/cljs"
+             :rules :cljs}]}
+
+  :cljsbuild
+  {:builds {:dev
+            {:source-paths ["target/src/cljs"]
+             :compiler {:output-to "resources/public/js/ibm80.js"
+                        :optimizations :whitespace
+                        :pretty-print true}}
+            :dev-test
+            {:source-paths ["target/test/cljs"]
+             :compiler {:output-to "target/test/js/ibm80-test.js"
+                        :optimizations :whitespace
+                        :pretty-print true}}}
+   :test-commands {"dev"
+                   ["phantomjs" :runner "target/test/js/ibm80-test.js"]}}
+)
--- test/cljx/ibm_80/core_test.cljx
+++ test/cljx/ibm_80/core_test.cljx
@@ -1,6 +1,10 @@
 (ns ibm-80.core-test
-  (:require [clojure.test :refer :all]
-            [ibm-80.core :refer :all]))
+  #+cljs
+  (:require-macros [cemerick.cljs.test :refer [deftest testing is are]])
+  (:require 
+   #+clj [clojure.test :refer [deftest testing is are]]
+   #+cljs [cemerick.cljs.test :as t]
+   [ibm-80.core :refer []]))
 
 (deftest a-test
   (testing "FIXME, I fail."
