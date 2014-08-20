a994306542234a1684fd5d2e9e7ae84a4f124ebd
Mark Simpson <verdammelt@gmail.com>
2014-08-20 14:33:31 -0400

    Initial commit of new project.
    This will be a Sorting Machine like the IBM-80.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ibm-80
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ibm_80/core_test.clj
@@ -0,0 +1,7 @@
+(ns ibm-80.core-test
+  (:require [clojure.test :refer :all]
+            [ibm-80.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
