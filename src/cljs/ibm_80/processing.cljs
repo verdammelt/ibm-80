5c975da984330b24984b917297ca88f21f3debc4
Mark Simpson <verdammelt@gmail.com>
2014-08-21 14:34:06 -0400

    Now talking with server OR client for same functionality.

--- src/cljs/ibm_80/processing.cljs
+++ src/cljs/ibm_80/processing.cljs
@@ -2,25 +2,39 @@
   (:require-macros [enfocus.macros :as em])
   (:require [enfocus.core :as ef]
             [enfocus.events :as ev]
+            [ajax.core :refer [POST GET]]
+            [goog.string :as gstring]
+            [goog.string.format :as gformat]
             [ibm-80.parser.record :refer [guess-delimeter]]))
 
+(defn- display-message [msg]
+  (ef/at "#messageArea" (ef/content msg)))
+
+(def ^:private message-format "The %s thinks the delimiter is: '%s'")
+
 (defn- process-on-client [data]
-  (str "The " (:processingLocation data) 
-       " thinks the delimiter is: '" 
-       (guess-delimeter (:testString data)) 
-       "'"))
+  (display-message 
+   (gstring/format message-format 
+                   (:processingLocation data) 
+                   (guess-delimeter (:testString data))) ))
 
 (defn- process-on-server [data]
-  "Cannot process on server yet (sorry).")
-
+  (POST "http://localhost:3000/sortblah" 
+        {:format :json
+         :params data
+         :error-handler #(display-message (str "An error occurred: " %))
+         :handler #(display-message 
+                    (gstring/format message-format 
+                                    "server"
+                                    (get % "delimiter")))}))
 
 (defn process [evt]
+  (.preventDefault evt)
   (let [form-data (ef/from "#sortingForm" (ef/read-form))
         processing-fn (case (:processingLocation form-data)
                         "server" process-on-server
                         "client" process-on-client)]
-    (ef/at "#messageArea" (ef/content (processing-fn form-data)))
-    (.preventDefault evt)))
+    (processing-fn form-data)))
 
 (defn ^:export init []
   (ef/at "#submit" (ev/listen :click process)))
